388b0a59e495ce55f80c9c90ba69d24a44293d6a
Lolvasit <108902921+Lolvasit@users.noreply.github.com>
2023-03-27 00:10:27 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+import json
+from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+# test bot
+BOT_TOKEN= "6113664519:AAHKj84tfeKiKP7qvoG21nVeepxwMTLqaJQ"
+
+ADMINS = [460936830]
